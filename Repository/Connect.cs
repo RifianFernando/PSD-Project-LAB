8efdb4152c0d57d6a873237cb88349f746c68802
WTaroki <william.tanjaya@binus.ac.id>
2023-05-10 16:15:28 +0700

    Rifian Ganteng

---
+++ Repository/Connect.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using KpopZtation.Model
+
+namespace KpopZtation.Repository
+{
+    public class Connect
+    {
+        public static KpopDatabaseEntities db = null;
+
+        public static KpopDatabaseEntities getInstance()
+        {
+            if(db == null)
+            {
+                db = new KpopDatabaseEntities();
+            }
+
+            return db;
+        }
+    }
+}
---
+++ View/RegisterPage.aspx
@@ -0,0 +1,15 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="RegisterPage.aspx.cs" Inherits="KpopZtation.View.RegisterPage" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <div>
+        </div>
+    </form>
+</body>
+</html>
