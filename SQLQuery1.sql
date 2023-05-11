bf0a8250f4540d212415ff3f1f9066e8cf21c08d
RFJST4REAL <rifianfernando19@gmail.com>
2023-05-11 22:01:12 +0700

    fix(mdf): fixing mdf to not include on repo

--- SQLQuery1.sql
+++
@@ -1,7 +0,0 @@
-﻿create table TransactionHeader(
-TransactionID INT PRIMARY KEY IDENTITY(2000000, 1) NOT NULL,
-TransactionDate date NOT NULL,
-CustomerID INT NOT NULL,
-
-FOREIGN KEY (CustomerID) REFERENCES Customer (CustomerID) ON UPDATE CASCADE ON DELETE CASCADE
-)
