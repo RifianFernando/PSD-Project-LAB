02b5e66e284cf04b6f0a13b0f108948a51ffcf8e
Dixon <dixonwillow23@gmail.com>
2023-05-11 21:50:51 +0700

    Album and Artist Handler, Repos, and Factory

---
+++ Factory/AlbumFactory.cs
@@ -0,0 +1,27 @@
+﻿using KpopZtation.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace KpopZtation.Factory
+{
+    public class AlbumFactory
+    {
+
+        public static Album CreateAlbum(int ArtID, String Name, String Image, int Price, int Stock, String Desc)
+        {
+            Album ab = new Album();
+
+            ab.ArtistID = ArtID;
+            ab.AlbumName = Name;
+            ab.AlbumImage = Image;
+            ab.AlbumPrice = Price;
+            ab.AlbumStock = Stock;
+            ab.AlbumDescription = Desc;
+
+            return ab;
+        }
+
+    }
+}
