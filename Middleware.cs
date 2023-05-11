459a5be9cb18be61616c09b6d5a18b820c13a6da
RFJST4REAL <rifianfernando19@gmail.com>
2023-05-12 00:04:45 +0700

    feat(middlware): add middleware function for session validateion

---
+++ Middleware.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace KpopZtation.Middleware
+{
+    public class Middleware
+    {
+        public static void isLogin()
+        {
+            if(Session["User"] == null)
+            {
+                Response.Redirect("LoginPage.aspx");
+            }
+
+            return;
+        }
+    }
+}
--- View/InsertAlbumPage.aspx.cs
+++ View/InsertAlbumPage.aspx.cs
@@ -4,14 +4,15 @@ using System.Linq;
 using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
+using KpopZtation.Middleware;
 
 namespace KpopZtation.View
 {
-    public partial class InsertAlbumPage : System.Web.UI.Page
+    public partial class InsertAlbumPage : Page
     {
         protected void Page_Load(object sender, EventArgs e)
         {
-
+            SessionMiddleware.isLogin(Page);
         }
 
         protected void InsertAlbumButton_Click(object sender, EventArgs e)
@@ -20,7 +21,7 @@ namespace KpopZtation.View
             String AlbumDesc = InsertAlbumDescription.Text;
             String AlbumPrice = InsertAlbumPrice.Text;
             String AlbumImage = FileUpload.FileName;
-            String AlbumStock = InsertAlbumStock.Text;            
+            String AlbumStock = InsertAlbumStock.Text;
         }
     }
 }
