db49d35b6727f37279f53e06394bdb1d43f61e4d
RFJST4REAL <rifianfernando19@gmail.com>
2023-05-11 22:32:00 +0700

    add: userCookie and session for 7 days

--- Middleware/Middleware.cs
+++ Middleware/Middleware.cs
@@ -7,5 +7,14 @@ namespace KpopZtation.Middleware
 {
     public class Middleware
     {
+        public static void isLogin()
+        {
+            if(Session["User"] == null)
+            {
+                Response.Redirect("LoginPage.aspx");
+            }
+
+            return;
+        }
     }
 }
