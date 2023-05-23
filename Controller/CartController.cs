75ced9e12e54617c5b155f5efb4e51b9a7e5fadf
WTaroki <william.tanjaya@binus.ac.id>
2023-05-23 20:00:18 +0700

    Added Changes in Cart Controller

--- Controller/CartController.cs
+++ Controller/CartController.cs
@@ -22,14 +22,11 @@ namespace KpopZtation.Controller
             CartHandler.InsertCart(CustomerID, AlbumID, Quantity);
             return;
         }
-<<<<<<< HEAD
 
         public static List<Cart> GetAllCartData(int id)
         {
             return CartHandler.GetAllCartData(id);
         }
 
-=======
->>>>>>> ea0d03a7305b988ce811aa449433b2b938eb2493
     }
 }
